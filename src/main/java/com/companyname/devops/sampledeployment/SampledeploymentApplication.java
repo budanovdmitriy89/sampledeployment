d5002b487d593f175e849121fa470c911ce0ad52
kleonov <kleonov@rencredit.ru>
2025-04-29 15:12:27 +0300

    Initial commit

---
+++ src/main/java/com/companyname/devops/sampledeployment/SampledeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.companyname.devops.sampledeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampledeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampledeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/companyname/devops/sampledeployment/SampledeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.companyname.devops.sampledeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampledeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
